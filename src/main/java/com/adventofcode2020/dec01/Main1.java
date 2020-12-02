33a2e59975c7791a2cb6be7bd840e3f9e2abbcca
Josh Lessard <josh.lessard@gmail.com>
2020-12-02 00:41:43 -0500

    Update Dec 01 to use binary search to cut down on naive iteration.

--- src/main/java/com/adventofcode2020/dec01/Main1.java
+++ src/main/java/com/adventofcode2020/dec01/Main1.java
@@ -4,8 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.binarySearch;
 import static java.util.stream.Collectors.toCollection;
 
 public class Main1 {
@@ -14,14 +16,10 @@ public class Main1 {
         List<Integer> expenses = getSortedExpenses();
         for ( int i = 0; i < expenses.size() - 1; ++i ) {
             int expense1 = expenses.get( i );
-            for ( int j = i + 1; j < expenses.size(); ++j ) {
-                int expense2 = expenses.get( j );
-                if ( expense1 + expense2 == 2020 ) {
-                    System.out.println( "Answer: " + (expense1 * expense2) );
-                    System.exit( 0 );
-                } else if ( expense1 + expense2 > 2020 ) {
-                    break;
-                }
+            int expense2 = 2020 - expense1;
+            if ( binarySearch( expenses, expense2 ) >= 0 ) {
+                System.out.println( "Answer: " + (expense1 * expense2) );
+                break;
             }
         }
     }
--- src/main/java/com/adventofcode2020/dec01/Main2.java
+++ src/main/java/com/adventofcode2020/dec01/Main2.java
@@ -4,26 +4,25 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.binarySearch;
 import static java.util.stream.Collectors.toCollection;
 
 public class Main2 {
 
     public static void main( String[] args ) throws Exception {
         List<Integer> expenses = getSortedExpenses();
+        outer:
         for ( int i = 0; i < expenses.size() - 2; ++i ) {
             int expense1 = expenses.get( i );
             for ( int j = i + 1; j < expenses.size() - 1; ++j ) {
                 int expense2 = expenses.get( j );
-                for ( int k = j + 1; k < expenses.size(); ++k ) {
-                    int expense3 = expenses.get( k );
-                    if ( expense1 + expense2 + expense3 == 2020 ) {
-                        System.out.println( "Answer: " + (expense1 * expense2 * expense3) );
-                        System.exit( 0 );
-                    } else if ( expense1 + expense2 + expense3 > 2020 ) {
-                        break;
-                    }
+                int expense3 = 2020 - expense1 - expense2;
+                if ( binarySearch( expenses, expense3 ) >= 0 ) {
+                    System.out.println( "Answer: " + (expense1 * expense2 * expense3) );
+                    break outer;
                 }
             }
         }
