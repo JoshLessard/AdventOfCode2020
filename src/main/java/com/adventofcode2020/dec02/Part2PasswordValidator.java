8d32e4355b9bfdfac1cedc859967d182b4566d5f
Josh Lessard <josh.lessard@gmail.com>
2020-12-03 00:57:01 -0500

    Clean up Dec 02 by using XOR.

--- src/main/java/com/adventofcode2020/dec02/Part2PasswordValidator.java
+++ src/main/java/com/adventofcode2020/dec02/Part2PasswordValidator.java
@@ -4,14 +4,6 @@ class Part2PasswordValidator implements PasswordValidator {
 
     @Override
     public boolean isValid( int position1, int position2, char letter, String password ) {
-        int numMatches = 0;
-        if ( password.charAt( position1 - 1 ) == letter ) {
-            ++numMatches;
-        }
-        if ( password.charAt( position2 - 1 ) == letter ) {
-            ++numMatches;
-        }
-
-        return numMatches == 1;
+        return password.charAt( position1 - 1 ) == letter ^ password.charAt( position2 - 1 ) == letter;
     }
 }
